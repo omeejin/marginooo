da3933a1128f46bbfe7a41c9cdcb99ebd8e4d662
omeejin <omeejin@gmail.com>
2025-04-27 21:40:04 +0900

    에러 제거 및 장기투자 업데이트

--- script.js
+++ script.js
@@ -6,7 +6,3 @@ function onClick() {
 }
 
 hamburger.addEventListener("click", onClick);
-
-document.querySelector("#nextBtn").addEventListener("click", function () {
-  document.querySelector(".carousel-container").style = "margin-left: -100vw";
-});
